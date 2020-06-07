7b973d754b3a0653131e23038b66055ffb83a3bd
dictuantran <39492119+dictuantran@users.noreply.github.com>
2020-06-07 15:23:12 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,79 +1,44 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"flag"
 	"log"
 	"net/http"
+	"strings"
 
-	"github.com/gorilla/mux"
+	"github.com/dictuantran/Tasks/config"
+	"github.com/dictuantran/Tasks/views"
 )
 
-// Article - Our struct for all articles
-type Article struct {
-	ID      string `json:"Id"`
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
-
-var Articles []Article
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
-func returnAllArticle(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: ReturnAllArticle")
-	json.NewEncoder(w).Encode(Articles)
-}
-
-func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["id"]
-
-	for _, article := range Articles {
-		if article.ID == key {
-			json.NewEncoder(w).Encode(article)
-		}
-	}
-}
-
-func createNewArticle(w http.ResponseWriter, r *http.Request) {
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	var article Article
-	json.Unmarshal(reqBody, &article)
-
-	Articles = append(Articles, article)
-	json.NewEncoder(w).Encode(article)
-}
-
-func deleteArticle(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	for index, article := range Articles {
-		if article.ID == id {
-			Articles = append(Articles[:index], Articles[index+1:]...)
-		}
-	}
-}
-
-func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/articles", returnAllArticle)
-	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
-	log.Fatal(http.ListenAndServe(":10000", myRouter))
-}
-
 func main() {
-	Articles = []Article{
-		Article{ID: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{ID: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	values, err := config.ReadConfig("config.json")
+	var port *string
+
+	if err != nil {
+		port = flag.String("port", "", "IP address")
+		flag.Parse()
+
+		//User is expected to give :8080 like input, if they give 8080
+		//we'll append the required ':'
+		if !strings.HasPrefix(*port, ":") {
+			*port = ":" + *port
+			log.Println("port is " + *port)
+		}
+
+		values.ServerPort = *port
 	}
-	handleRequests()
+
+	views.PopulateTemplates()
+
+	// Login logout
+	http.HandleFunc("/login/", views.LoginFunc)
+
+	// these handlers are used for restoring tasks
+	http.HandleFunc("/", views.RequiresLogin(views.ShowAllTasksFunc))
+	http.HandleFunc("/add/", views.RequiresLogin(views.AddTaskFunc))
+
+	http.Handle("/static/", http.FileServer(http.Dir("public")))
+
+	log.Println("running server on ", values.ServerPort)
+	log.Fatal(http.ListenAndServe(values.ServerPort, nil))
 }
